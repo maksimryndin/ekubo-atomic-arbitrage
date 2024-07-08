dc9d03fc366380812489a20971123aa7aca6e014
Maksim Ryndin <maksim.ryndin@gmail.com>
2024-07-08 16:50:56 +0000

    calls

--- atomic-arbitrage/src/main.rs
+++ atomic-arbitrage/src/main.rs
@@ -11,7 +11,8 @@ use starknet::{
 use std::cmp::Reverse;
 use std::env;
 use std::iter;
-use tracing::{error, info};
+use tokio::time::{sleep, Duration};
+use tracing::{debug, error, info};
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 struct ArbitrageOpportunity {
@@ -36,7 +37,7 @@ async fn check_arbitrage(
             e
         })
         .ok()?;
-    info!("quotes for amount {amount}");
+    debug!("quotes for amount {amount}:\n{quotes:#?}");
     let total = quotes.total;
     (total > amount + min_profit && !quotes.splits.is_empty()).then(|| ArbitrageOpportunity {
         amount,
@@ -45,50 +46,49 @@ async fn check_arbitrage(
     })
 }
 
-fn multihop_swap(
-    split: Quote,
-    router_address: Felt,
-    token_address: Felt,
-    multihop_swap_selector: Felt,
-) -> Call {
-    let specified_amount = split.specified_amount;
-    let calldata: Vec<Felt> = iter::once(Felt::from(split.route.len()))
-        .chain(split.route.into_iter().flat_map(|node| {
-            let RouteNode {
-                pool_key:
-                    PoolKey {
-                        token0,
-                        token1,
-                        fee,
-                        tick_spacing,
-                        extension,
-                    },
-                sqrt_ratio_limit,
-                skip_ahead,
-            } = node;
-            [
+fn node_to_array(node: RouteNode) -> [Felt; 8] {
+    let RouteNode {
+        pool_key:
+            PoolKey {
                 token0,
                 token1,
                 fee,
-                tick_spacing.into(),
+                tick_spacing,
                 extension,
-                Felt::from(sqrt_ratio_limit.to_bigint() % Felt::from(2u8).pow(128u8).to_bigint()),
-                Felt::from(sqrt_ratio_limit.to_bigint() >> 128),
-                skip_ahead,
-            ]
-        }))
+            },
+        sqrt_ratio_limit,
+        skip_ahead,
+    } = node;
+    [
+        token0,
+        token1,
+        fee,
+        tick_spacing.into(),
+        extension,
+        Felt::from(sqrt_ratio_limit.to_bigint() % Felt::from(2u8).pow(128u8).to_bigint()),
+        Felt::from(sqrt_ratio_limit.to_bigint() >> 128),
+        skip_ahead,
+    ]
+}
+
+fn call_data_for_split(split: Quote, token_address: Felt) -> impl Iterator<Item = Felt> {
+    let specified_amount = split.specified_amount;
+    iter::once(Felt::from(split.route.len()))
+        .chain(split.route.into_iter().flat_map(node_to_array))
         .chain(iter::once(token_address))
         .chain(iter::once(specified_amount))
         .chain(iter::once(Felt::ZERO))
-        .collect();
-
-    Call {
-        to: router_address,
-        selector: multihop_swap_selector,
-        calldata,
-    }
 }
 
+fn call_data_for_multisplit(splits: Vec<Quote>, token_address: Felt) -> impl Iterator<Item = Felt> {
+    iter::once(Felt::from(splits.len())).chain(
+        splits
+            .into_iter()
+            .flat_map(move |split| call_data_for_split(split, token_address)),
+    )
+}
+
+#[allow(unreachable_code)]
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
     dotenvy::dotenv()?;
@@ -121,57 +121,72 @@ async fn main() -> Result<()> {
     let max_hops: u8 = env::var("MAX_HOPS")?.parse()?;
     let min_profit = Felt::from_dec_str(&env::var("MIN_PROFIT")?)?;
     let num_top_quotes: usize = env::var("NUM_TOP_QUOTES_TO_ESTIMATE")?.parse()?;
-    //loop {
-    let mut opportunities: Vec<ArbitrageOpportunity> =
-        join_all(amounts_to_quote.iter().map(|&amount| {
-            check_arbitrage(
-                &client,
-                amount,
-                min_profit,
-                &token_address_hex,
-                max_splits,
-                max_hops,
-            )
-        }))
-        .await
-        .into_iter()
-        .flatten()
-        .collect();
-    opportunities.sort_unstable_by_key(|opportunity| Reverse(opportunity.profit));
-    let calls: Option<[Call; 3]> =
-        opportunities
+    let check_interval: u64 = env::var("CHECK_INTERVAL_MS")?.parse()?;
+    loop {
+        let mut opportunities: Vec<ArbitrageOpportunity> =
+            join_all(amounts_to_quote.iter().map(|&amount| {
+                check_arbitrage(
+                    &client,
+                    amount,
+                    min_profit,
+                    &token_address_hex,
+                    max_splits,
+                    max_hops,
+                )
+            }))
+            .await
             .into_iter()
-            .take(num_top_quotes)
-            .find_map(|opportunity| {
-                let transfer_call = Call {
-                    to: token_address,
-                    selector: transfer_selector,
-                    calldata: vec![router_address, opportunity.amount, Felt::ZERO],
-                };
+            .flatten()
+            .collect();
+        opportunities.sort_unstable_by_key(|opportunity| Reverse(opportunity.profit));
+        let calls: Option<[Call; 3]> =
+            opportunities
+                .into_iter()
+                .take(num_top_quotes)
+                .find_map(|opportunity| {
+                    let transfer_call = Call {
+                        to: token_address,
+                        selector: transfer_selector,
+                        calldata: vec![router_address, opportunity.amount, Felt::ZERO],
+                    };
 
-                let clear_profits_call = Call {
-                    to: router_address,
-                    selector: clear_minimum_selector,
-                    calldata: vec![token_address, opportunity.amount, Felt::ZERO],
-                };
+                    let clear_profits_call = Call {
+                        to: router_address,
+                        selector: clear_minimum_selector,
+                        calldata: vec![token_address, opportunity.amount, Felt::ZERO],
+                    };
 
-                let mut splits = opportunity.quotes.splits;
+                    let mut splits = opportunity.quotes.splits;
 
-                let call = if splits.len() == 1 {
-                    let split = splits.pop()?;
-                    if split.route.len() == 1 {
-                        error!("unexpected single hop route");
-                        return None;
-                    }
-                    multihop_swap(split, router_address, token_address, multihop_swap_selector)
-                } else {
-                    todo!()
-                };
+                    let call = if splits.len() == 1 {
+                        let split = splits.pop()?;
+                        if split.route.len() == 1 {
+                            error!("unexpected single hop route");
+                            return None;
+                        }
+                        Call {
+                            to: router_address,
+                            selector: multihop_swap_selector,
+                            calldata: call_data_for_split(split, token_address).collect(),
+                        }
+                    } else {
+                        Call {
+                            to: router_address,
+                            selector: multihop_swap_selector,
+                            calldata: call_data_for_multisplit(splits, token_address).collect(),
+                        }
+                    };
 
-                Some([transfer_call, call, clear_profits_call])
-            });
-
-    //}
+                    Some([transfer_call, call, clear_profits_call])
+                });
+        match calls {
+            Some(calls) => {
+                info!("Executing top arbitrage:\n{calls:#?}")
+            }
+            None => info!("No arbitrage found"),
+        }
+        sleep(Duration::from_millis(check_interval)).await;
+    }
 
     Ok(())
 }
